4365729a0b13f2ec6e5a87888f2e3955a6ed944c
sekcheong <scheong@gmail.com>
2017-07-13 14:49:30 -0500

    first

---
+++ SharpStreaming/TestClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Simon.SharpStreaming.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RtspClient rtspClient = new RtspClient();
+            bool result = rtspClient.ConnectServer("144.92.136.102", 554);
+            rtspClient.OpenStream("rtsp://admin:1675WisM%40d@144.92.136.102", "c:\\temp\\");
+        }
+    }
+}
